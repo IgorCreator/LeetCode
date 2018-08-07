e1d56e2bc066d4464897ddafa2e111d49a1446db
igas0817 <ihar.asamadzinau@NetCracker.com>
2018-08-07 14:16:06 +0300

    852. Peak Index in a Mountain Array

--- src/main/java/easy/MergeTwoBinaryTrees.java
+++ src/main/java/easy/MergeTwoBinaryTrees.java
@@ -10,24 +10,11 @@ public class MergeTwoBinaryTrees {
             treeNode = t2;
         } else if(t1 != null && t2 != null ){
             treeNode = new TreeNode(t1.val + t2.val);
-            treeNode.right = insertingNode(t1.right, t2.right, treeNode.right);
-            treeNode.left = insertingNode(t1.left, t2.left, treeNode.left);
-        }
-
-        return treeNode;
-    }
-
-    private TreeNode insertingNode(TreeNode node1, TreeNode node2, TreeNode treeNode) {
-        if(node2 == null && node1 != null){
-            treeNode = node1;
-        } else if(node1 == null && node2 != null){
-            treeNode = node2;
-        } else if(node1 != null && node2 != null) {
-            treeNode = mergeTrees(node1, node2);
+            treeNode.right = mergeTrees(t1.right, t2.right);
+            treeNode.left = mergeTrees(t1.left, t2.left);
         }
         return treeNode;
     }
-
 
     public TreeNode mergeTreesNotMineDecision(TreeNode t1, TreeNode t2) {
         if (t1 == null)
